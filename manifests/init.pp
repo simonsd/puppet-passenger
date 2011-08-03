b3872e3a4629efb7417fa0fa70613e031a37fa7a
Dave Simons <dave@inuits.be>
2011-08-03 10:32:00 +0200

    add some more compat towards centos6
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,16 @@ class passenger (
   # Package is available only for RedHat and CentOS 5 
   package {
     "rubygem-passenger": 
-       ensure => "2.2.2-1",
+       ensure => $operatingsystemrelease ? {
+            6.0 => "latest",
+            * => "2.2.2-1",
+       },
        require => Package["$webserver"],
        before => Service["$webserver"],
   }
 
   file {
-    "/etc/httpd/conf.d/passenger.conf":
+    "/etc/$webserver/conf.d/passenger.conf":
       source => "puppet:///modules/passenger/passenger.conf",
       mode => 644,
       owner=> root,
