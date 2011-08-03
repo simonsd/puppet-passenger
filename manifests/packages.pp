3988aa2a967d79fecd40eaa51742cec83a2dc085
Dave Simons <dave@inuits.be>
2011-08-03 10:42:31 +0200

    quote selection string for pkg version
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -2,8 +2,8 @@ class passenger::packages {
   package {
     "rubygem-passenger": 
        ensure => $operatingsystemrelease ? {
-            6.0 => "latest",
-            * => "2.2.2-1",
+            '6.0' => "latest",
+            '*' => "2.2.2-1",
        },
        require => Package["$webserver"],
        before => Service["$webserver"],
