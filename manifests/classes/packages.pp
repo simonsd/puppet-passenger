28dba27ba52434d572de55f81927a5b08d15bb29
Dave Simons <dave@inuits.be>
2011-08-08 11:39:19 +0200

    install from gem - at least that works
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -2,10 +2,12 @@ class passenger::packages {
   package {
     "rubygem-passenger": 
        ensure => $operatingsystemrelease ? {
-            '6.0' => "latest",
+            '6.0' => "2.2.7",
             '*' => "2.2.2-1",
        },
+       name => passenger,
+       provider => gem,
        require => Package["$webserver"],
-       before => Service["$webserver"],
+       before => Service["$webserver"];
   }
 }
