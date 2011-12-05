e4b63f3c63e55017bd770e531db5d7ecaba84114
Dave Simons <dave@inuits.be>
2011-12-05 15:58:17 +0100

    remove stages crap
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,25 +2,12 @@ import 'classes/*'
 
 class passenger (
 	$webserver = 'httpd',
-	$stages = 'no',
 	$version = '3.0.8'
 ) {
-	if $stages == 'no' {
-	    class {
-			'passenger::packages':
-				before => Class['passenger::config'];
-			'passenger::config':;
-		}
-	} else {
-		class {
-			'passenger::packages':
-				require => 'apache::packages',
-				stage => depends;
-			'passenger::config':
-				require => 'passenger::packages',
-				before => 'apache::service',
-				stage => config;
-		}
+    class {
+		'passenger::packages':
+			before => Class['passenger::config'];
+		'passenger::config':;
 	}
 
     Exec{path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'}
