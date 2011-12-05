4faae84771e2299fbc92c3f6182f1ede24a83856
Dave Simons <dave@inuits.be>
2011-12-05 17:53:16 +0100

    add curl-devel package for building passenger modules
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -1,14 +1,22 @@
 class passenger::packages {
   package {
     "rubygem-passenger": 
-       ensure => "$passenger::version",
-       name => passenger,
-       provider => gem,
-       require => $::operatingsystem ? {
-			default => Package["$passenger::webserver", "$passenger::webserver-devel"],
-			archlinux => Package["apache"],
-			debian => Package["apache"],
-       },
-       before => Service["$passenger::webserver"];
+      ensure => "$passenger::version",
+      name => passenger,
+      provider => gem,
+      require => $::operatingsystem ? {
+        default => Package["$passenger::webserver"],
+        archlinux => Package["apache"],
+        debian => Package["apache"],
+      },
+      before => Service["$passenger::webserver"];
+
+    'curl-devel':
+      ensure => installed,
+      name => $::operatingsystem ? {
+        archlinux => 'curl',
+        centos => 'libcurl-devel',
+        debian => 'libcurl4-openssl-dev',
+      };
   }
 }
