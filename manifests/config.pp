18db6571267a3d6227cdc6a3ed9bc573419ff70b
Dave Simons <dave@inuits.eu>
2012-01-25 01:42:35 +0100

    remove archlinux specific config stuff - fixed in apache config
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,8 +14,7 @@ class passenger::config {
   file {
     'passenger.conf':
       path      => $::operatingsystem ? {
-        archlinux => '/etc/httpd/conf/extra/passenger.conf',
-        centos    => '/etc/httpd/conf.d/passenger.conf',
+        default   => '/etc/httpd/conf.d/passenger.conf',
         debian    => '/etc/apache2/conf.d/passenger.conf',
       },
       content   => template('passenger/passenger.conf.erb'),
@@ -32,13 +31,5 @@ class passenger::config {
         debian  => "${ruby::lib_dir}/gems/${ruby::version}/gems/passenger-${passenger::version}/bin/passenger-install-apache2-module --auto",
       },
       creates => "${ruby::lib_dir}/gems/${ruby::version}/gems/passenger-${passenger::version}/ext/apache2/mod_passenger.so";
-  }
-
-  if $::operatingsystem == 'archlinux' {
-    exec {
-      'include passenger.conf':
-        command => 'echo -e "\n# Passenger config\nInclude conf/extra/passenger.conf" >> /etc/httpd/conf/httpd.conf',
-        require => File['passenger.conf', 'apache.conf'];
-    }
   }
 }
