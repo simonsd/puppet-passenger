7aa7e9133a146a00dc4430041d4ed291338f2cd1
Dave Simons <dave@inuits.eu>
2013-01-16 15:11:09 +0100

    fixup inclusion/ordering syntax
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 class passenger (
   $version = '3.0.8'
 ) {
-  class { '::ruby': }
-  class { 'passenger::packages':
-    require => Class['::ruby'],
-  }
-  class { 'passenger::config':
-    require => Class['passenger::packages'],
-  }
+  include ::ruby
+  include ::passenger::packages
+  include ::passenger::config
+
+  Class['::ruby'] ->
+  Class['::passenger::packages'] ->
+  Class['::passenger::config']
 
   Exec{ path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin', }
 }
